bb951defd1d0214e91a6e2ca5decbf0e24f44622
SUDIPTO1903 <sududead@gmail.com>
2026-07-19 02:43:01 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/19/2026, 2:43:01 AM
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        vector<int> left(n, 1);
+        vector<int> right(n, 1);
+
+        // Left to right
+        for (int i = 1;i<n;i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+        // Right to left
+        for (int i=n - 2;i>= 0;i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+        int totalCandies = 0;
+        for (int i = 0; i < n; i++) {
+            totalCandies += max(left[i], right[i]);
+        }
+        return totalCandies;
+    }
+};
